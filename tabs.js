9b9cc83b71796054036b8f021791d3b5701b1b1a
frontendella <ryukhinaella@gmail.com>
2023-04-08 11:23:50 -0700

    Changing the image on tabs switch

--- tabs.js
+++ tabs.js
@@ -1,15 +1,19 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
-
 tabList.addEventListener('keydown', changeTabFocus);
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', changeTabPanel);
+});
+
 
 let tabFocus = 0;
 function changeTabFocus(e) {
     const keydownLeft = 37;
     const keydownRight = 39;
     
-    if (e.keyCode === keydownLeft || e.keyCode === keydownRig ht) {
+    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
         tabs[tabFocus].setAttribute("tabindex", -1);
     }
     
@@ -29,4 +33,29 @@ function changeTabFocus(e) {
     
     tabs[tabFocus].setAttribute("tabindex", 0);
     tabs[tabFocus].focus();
+}
+
+
+function changeTabPanel(e) {
+    const targetTab = e.target;
+    const targetPanel = targetTab.getAttribute("aria-controls");
+    const targetImage = targetTab.getAttribute("data-image")
+
+    const tabContainer = targetTab.parentNode;
+    const mainContainer = tabContainer.parentNode;
+    
+    mainContainer
+        .querySelectorAll('[role="tabpanel"]')
+        .forEach((panel) => panel.setAttribute("hidden", true));
+    
+    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+    
+    mainContainer
+    .querySelectorAll('picture')
+    .forEach((picture) => picture.setAttribute("hidden", true));
+
+    mainContainer.querySelector([`#${targetImage}`]).removeAttribute('hidden');
+
+    
+    // console.log(mainContainer);
 }
